8a0f024906e14df980d1483c4b6d240c2bf550b4
demetri <demetri.bairaktaris@imanage.com>
2020-06-07 15:33:36 -0500

    Initial Commit

---
+++ drive_native/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  drive_native
+//
+//  Created by admin on 6/7/20.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
